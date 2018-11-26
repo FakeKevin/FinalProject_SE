82f74812180f783a857b2ea5b7a0b4b245bba000
GitterAlex <junglejim1911@gmail.com>
2018-11-25 20:54:38 -0500

    created pom.xml

--- SEFinal/src/ca/sheridancollege/beans/Person.java
+++ SEFinal/src/ca/sheridancollege/beans/Person.java
@@ -9,9 +9,11 @@ import javax.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 @Entity
 @Getter
